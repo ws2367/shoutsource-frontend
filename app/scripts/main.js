88fd93519aaa33884116c09a350b9e06864151c5
Mark Liang <mark.liang@akqa.com>
2016-05-19 00:58:15 +0800

    Update content & styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,10 @@
 // jshint devel:true
 console.log('\'Allo \'Allo!');
+
+$(".button, .scroll").click(function(e) {
+	e.preventDefault();
+	var link = $(this).parent().attr('href');
+    $('html, body').animate({
+        scrollTop: $(link).offset().top
+    }, 1000, 'easeInOutQuart');
+});
