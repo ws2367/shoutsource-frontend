b6291fa6dd212e93a0d87387fd12ff5d63d654fe
Mark Liang <mark.liang@akqa.com>
2016-05-19 03:04:28 +0800

    Add Form design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,26 @@
 // jshint devel:true
 console.log('\'Allo \'Allo!');
 
-$(".button, .scroll").click(function(e) {
-	e.preventDefault();
-	var link = $(this).parent().attr('href');
-    $('html, body').animate({
-        scrollTop: $(link).offset().top
-    }, 1000, 'easeInOutQuart');
+$(document).ready(function(){
+	$(".button, .scroll").click(function(e) {
+		e.preventDefault();
+		var link = $(this).parent().attr('href');
+	    $('html, body').animate({
+	        scrollTop: $(link).offset().top
+	    }, 1000, 'easeInOutQuart');
+	});
+
+	var h = 60 + $('.image').height()/2-35;
+	console.log(h);
+	$(".swiper-button-white").css({
+		"bottom":h+"px",
+	});
+})
+
+$(window).resize(function(){
+	var h = 60 + $('.image').height()/2-35;
+	console.log(h);
+	$(".swiper-button-white").css({
+		"bottom":h+"px",
+	});
 });
