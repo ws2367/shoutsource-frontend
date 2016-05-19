ac6d6e811b7f51cc4f9704699391ac7ff9cf6dea
Mark Liang <big.head.cool@gmail.com>
2016-05-19 17:43:25 +0800

    Add page fade in

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,8 @@
 console.log('\'Allo \'Allo!');
 
 $(document).ready(function(){
+	$('body').css('opacity', 1);
+
 	$(".button, .scroll").click(function(e) {
 		e.preventDefault();
 		var link = $(this).parent().attr('href');
@@ -11,11 +13,12 @@ $(document).ready(function(){
 	    return false;
 	});
 
-	var h = 60 + $('.image').height()/2-35;
-	console.log(h);
-	$(".swiper-button-white").css({
-		"bottom":h+"px",
-	});
+	
+		var h = 60 + $('.image').height()/2-35;
+		console.log(h);
+		$(".swiper-button-white").css({
+			"bottom":h+"px",
+		});
 })
 
 $(window).resize(function(){
